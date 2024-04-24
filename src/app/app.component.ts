93e8168709b38501ee7c953a6bbfce4e3bd008ed
Andrei Floricel <andrei.floricel@gmail.com>
2024-04-24 13:51:24 +0300

    Add sandbox for Zendesk issue #945

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { GridOptions, Module } from '@ag-grid-community/core';
 import {
+  ActionColumnContext,
   AdaptableApi,
+  AdaptableButton,
   AdaptableOptions,
 } from '@adaptabletools/adaptable-angular-aggrid';
 import { rowData } from './rowData';
@@ -23,6 +25,35 @@ export class AppComponent {
     userName: 'demo-user',
     // licenseKey: <add_provided_license_key>,
     adaptableId: 'AdapTable Angular App',
+    actionColumnOptions: {
+      actionColumns: [
+        {
+          columnId: 'action',
+          friendlyName: 'action',
+          actionColumnButton: [
+            {
+              label: '',
+              icon: {
+                src: '../assets/adaptable_icon.svg',
+                style: { height: 25, width: 25 },
+              },
+              hidden: (
+                button: AdaptableButton<ActionColumnContext>,
+                context: ActionColumnContext
+              ) => {
+                return false;
+              },
+              onClick: (
+                button: AdaptableButton<ActionColumnContext>,
+                context: ActionColumnContext
+              ) => {
+                console.log('Action');
+              },
+            },
+          ],
+        },
+      ],
+    },
     predefinedConfig: {
       Dashboard: {
         Revision: Date.now(),
@@ -30,6 +61,39 @@ export class AppComponent {
           {
             Name: 'Default',
             Toolbars: ['Layout', 'Query'],
+          },
+        ],
+      },
+      Layout: {
+        Revision: Date.now(),
+        CurrentLayout: 'Default',
+        Layouts: [
+          {
+            Name: 'Default',
+            Columns: [
+              'name',
+              'language',
+              'github_stars',
+              'license',
+              'week_issue_change',
+              'created_at',
+              'has_wiki',
+              'updated_at',
+              'pushed_at',
+              'github_watchers',
+              'description',
+              'open_issues_count',
+              'closed_issues_count',
+              'open_pr_count',
+              'closed_pr_count',
+              'has_projects',
+              'has_pages',
+              'topics',
+              'action',
+            ],
+            PinnedColumnsMap: {
+              action: 'right',
+            },
           },
         ],
       },
